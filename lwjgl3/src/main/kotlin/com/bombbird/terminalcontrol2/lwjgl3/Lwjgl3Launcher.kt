20c9ba3e6c35be1a8cc8a518a4ae64797fc85cb5
Shang Yi Ku <kushangyi2001@gmail.com>
2025-08-23 13:19:12 +0800

    Remove unneeded modules, libraries and dependencies

--- lwjgl3/src/main/kotlin/com/bombbird/terminalcontrol2/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/com/bombbird/terminalcontrol2/lwjgl3/Lwjgl3Launcher.kt
@@ -6,10 +6,12 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import com.bombbird.terminalcontrol2.TerminalControl2
 import com.bombbird.terminalcontrol2.integrations.StubAchievementHandler
+import com.bombbird.terminalcontrol2.integrations.StubDiscordHandler
+import com.bombbird.terminalcontrol2.sounds.StubTextToSpeech
 
 /** Launches the desktop (LWJGL3) application. */
 fun main() {
-    Lwjgl3Application(TerminalControl2(DesktopFileHandler(), DesktopTTSHandler(), DesktopDiscordHandler(), StubAchievementHandler),
+    Lwjgl3Application(TerminalControl2(DesktopFileHandler(), StubTextToSpeech, StubDiscordHandler, StubAchievementHandler),
         Lwjgl3ApplicationConfiguration().apply {
             setTitle("Terminal Control 2")
             if (!isLinuxWayland()) {
